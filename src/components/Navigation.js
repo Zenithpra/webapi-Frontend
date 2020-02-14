f3aa5ea6ad3818825e4ae91274485128445896d9
Zenithpra <49400681+Zenithpra@users.noreply.github.com>
2020-02-14 13:39:19 +0545

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Button, Navbar, Nav, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink } from 'reactstrap'
+import { withRouter } from 'react-router-dom'
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleLogout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem('token');
+        this.props.history.push('/')
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+    render() {
+        return (
+            <Navbar color='dark' dark expand='md'>
+                <NavbarBrand href='/dashboard'>Footsal Management</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className='mr-auto' navbar>
+                        <NavItem>
+                            <NavLink href='/dashboard'>Dashboard</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/dashboard'>Booking</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/dashboard'>Events</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/profile'>Profile</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <Button color='warning' onClick={this.handleLogout}> Logout</Button>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default withRouter(Navigation)
+
